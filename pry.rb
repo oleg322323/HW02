786c7fb94fee9fc840b5f4a765101c015c3aabfc
Oleg Gubskiy <oleg322323@gmail.com>
2016-02-04 04:04:37 +0200

    t

--- pry.rb
+++ pry.rb
@@ -1,4 +1,21 @@
 require 'pry'
 
-qwe = 12
+a=1
+b=2
+if a==b 
+	puts 'а и б равно'
+end
+
+
+
 binding.pry
+qwe = 12
+@qwe=13
+@@qwe=11
+$qwe=123
+
+
+def qwe
+	qweeweqweq=9
+	Cqwrqdwa = 7
+end
---
+++ pry2.rb
@@ -0,0 +1,5 @@
+require 'pry'
+
+ARGV
+
+binding.pry
